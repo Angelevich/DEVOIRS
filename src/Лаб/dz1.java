cc3ea626102ba2f0ece7c5503b3f7b923f796ce0
SUPER <angelevich@mail.ru>
2022-04-16 14:17:31 +0300

    домашка 1

---
+++ src/Лаб/dz1.java
@@ -0,0 +1,13 @@
+package Лаб;
+
+public class dz1 {
+    public static void main(String[] args) {
+        int a, b, c, sum;
+        a=10;
+        b=15;
+        c=20;
+        sum = (a + b + c);
+        System.out.println ( "Итого=");
+        System.out.println (sum);
+    }
+}
